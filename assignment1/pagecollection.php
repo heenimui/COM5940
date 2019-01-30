209b24bbfc301bd51e22d33f1d1e3ceaed0575ab
张崇静 <18811722966@163.com>
2019-01-30 20:34:44 +0800

    correct href error

--- assignment1/pagecollection.php
+++ assignment1/pagecollection.php
@@ -6,7 +6,7 @@
 				<div class="container">
 					<div class="row no-gutters slider-text justify-content-center align-items-center">
 	          <div class="col-md-8 ftco-animate">
-	            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Collection</span></p>
+	            <p class="breadcrumbs"><span class="mr-2"><a href="http://dev-chongjing.pantheonsite.io/">Home</a></span> <span>Collection</span></p>
 	            <h1 class="bread">My Collection</h1>
 	          </div>
 	        </div>
@@ -19,15 +19,16 @@
                   while (have_posts()) :
                        the_post(); ?>
 						<div class="col-md-4 ftco-animate">
-							<a href="images/image_1.jpg" class="photography-entry img image-popup d-flex justify-content-start align-items-end" style="background-image: url(images/image_1.jpg);">
+							<a href="images/image_1.jpg" class="photography-entry img image-popup d-flex justify-content-start align-items-end" style="background-image: url(<?php echo get_theme_file_uri('images/image_1.jpg')?>);">
 								<div class="overlay"></div>
 								<div class="text ml-4 mb-4">
-									<h3><?php the_content(); ?></h3>
-									<span class="tag"><?php the_title(); ?></span>
+									<h3><?php the_content();?></h3>
+									<span class="tag"><?php the_title();?></span>
 								</div>
 							</a>
 						</div>
-
+          <?php endwhile;
+          endif; ?>
 					</div>
 				</div>
 			</section>
